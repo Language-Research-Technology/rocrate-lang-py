88d6ab069732bb4f6357816d9a1919f9756415af
Moises Sacal <moisbo@gmail.com>
2021-10-07 13:15:13 +1100

    updated version tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     required = f.read().splitlines()
 
-version = '0.0.1'
+version = 'v0.0.1.dev'
 
 setup(
     name='rocrate_lang',
@@ -21,8 +21,8 @@ setup(
     python_requires='>=3.6',
     license="gplv2",
     url='https://github.com/Language-Research-Technology/rocrate-lang-py',
-    #download_url=('https://github.com/Language-Research-Technology/rocrate-lang-py/archive/'
-    #              f'{version}.tar.gz'),
+    download_url=('https://github.com/Language-Research-Technology/rocrate-lang-py/archive/'
+                  f'{version}.tar.gz'),
     keywords="atap ldaca rocrates",
     install_requires=[required]
 )
